194fb30c11e3765842a393ec9c405b446cb14946
joaopedro3001 <beuter.joao@escola.pr.gov.br>
2025-07-10 18:51:07 -0300

    Add files via upload

---
+++ Flashcard-Final-main/Flashcard Página Web/flashcard/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Futebol',
+    ' Quais países vão sediar a copa do mundo de 2026?',
+    'Estados Unidos, México e Canadá.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o O Ponto Mais Profundo da terra? ',
+    'A Fossa das Marianas, localizada no Oceano Pacífico, é o ponto mais profundo da Terra, atingindo cerca de 11.034 metros abaixo do nível do mar. Se o Monte Everest fosse colocado lá, sua ponta ainda estaria submersa.'
+)
+
+criaCartao(
+    'Games',
+    'Quais são os 5 jogos mais vendidos do mundo?',
+    'Minecraft, Grand Theft Auto V, Wii Sports, Tetris e Mario Kart 8 / Deluxe. '
+)
+
+criaCartao(
+    'Filmes',
+    'Quantos filems existem no mundo todo?',
+    'Acredita-se que existam mais de 500.000 filmes, incluindo longas-metragens, curtas e documentários.  '
+)
+
+criaCartao(
+    'Tecnologia',
+    'Qual foi o primeiro Smartphone?',
+    ' O primeiro smartphone, conhecido como Simon Personal Communicator, foi lançado pela IBM em 1994, apresentando funções como calendário, agenda e jogos, antecipando a revolução dos dispositivos móveis.'
+)
